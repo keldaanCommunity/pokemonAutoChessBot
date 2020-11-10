3ab3ccc7d207694ab6e936cebd397a6ca25317e0
Keldaan <arnaudgregoire92@gmail.com>
2020-11-10 18:44:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.login(process.env.BOT_TOKEN);
+
+const prefix = "!";
+
+client.on("message", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Bzz bzz, this message had a latency of ${timeTaken} ms.`);
+  }
+});
