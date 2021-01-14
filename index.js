ed7b32c5a851cd11cb15897677676cb6bc458501
arnaudgregoire <arnaudgregoire92@gmail.com>
2021-01-14 10:15:09 +0100

    activity command

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Discord = require("discord.js");
 const Mongoose = require("mongoose");
 const client = new Discord.Client();
 const User = require('@colyseus/social').User;
+const QuickChart = require('quickchart-js');
+const GameStats = require('./game-stats');
 
 client.login(process.env.BOT_TOKEN);
 
@@ -22,7 +24,92 @@ client.on("message", function(message) {
   }
 
   else if (command === "help") {
-    message.reply(`Bzz bzz, I am a Pokemon Auto Chess bot. Im currently under development. !help, !ping !stats !whoisthebestdev !leaderboard`);
+    message.reply(`Bzz bzz, I am a Pokemon Auto Chess bot. Im currently under development. !help, !ping !stats !whoisthebestdev !leaderboard !activity`);
+  }
+
+  else if(command == "activity"){
+    let days = [0,0,0,0,0,0,0,0,0,0];
+    let daysString = ['','','','','','','','','','']; 
+    Mongoose.connect(process.env.MONGO_URI , (err) => {
+      let now = Date.now();
+      GameStats.find({'time': { $gt: now - 86400000 * 10 }}, async (err, datas)=> {
+        for (let i = 10; i > 0; i--) {
+          let dateHier;
+          let dateDemain;
+          datas.forEach(data=>{
+            let dateTime = new Date(data.time);
+            dateHier = new Date(now - 86400000 * i);
+            dateDemain = new Date(now - 86400000 * i + 86400000);
+            //console.log("date  : ",dateTime.toUTCString());
+            //console.log("hier  : ",dateHier.toUTCString());
+            //console.log("demain: ",dateDemain.toUTCString());
+            if(dateTime > dateHier && dateTime < dateDemain){
+              //console.log('true');
+              days[10-i] += 1;
+            }
+          });
+          daysString[10-i] = dateDemain.toUTCString().slice(0,9);
+        }
+        //console.log(days);
+        const chart = new QuickChart();
+        chart.setBackgroundColor('rgba(54, 57, 63, 1)');
+        chart
+          .setConfig({
+            type: 'line',
+            data:
+            {
+                labels: daysString,
+                datasets: [{
+                  label: 'Games played',
+                    data: days,
+                    backgroundColor:'rgba(104, 130, 158, 0.5)',
+                    borderColor:'rgba(89,130,52,1)',
+                    pointBackgroundColor:'rgba(174,189,56,1)'
+                }]
+            },
+            options:{
+              legend:{
+                labels:{
+                  fontColor: 'white'
+                }
+              },
+            scales: {
+              yAxes: [{
+                  ticks: {
+                      fontColor: "white",
+                  }
+              }],
+              xAxes: [{
+                  ticks: {
+                      fontColor: "white",
+                  }
+              }]
+            }
+
+            }
+          })
+        const url = await chart.getShortUrl();
+        const exampleEmbed = {
+          color: 0x0099ff,
+          title: 'Game Activity',
+          author: {
+            name: 'Pokemon Auto Chess',
+            icon_url:  'https://raw.githubusercontent.com/arnaudgregoire/pokemonAutoChess/master/app/public/dist/assets/ui/logo-pac.png',
+            url: 'https://pokemon-auto-chess.herokuapp.com/',
+          },
+          image: {
+            url: url
+          },
+          timestamp: new Date(),
+          footer: {
+            text: 'Pokemon Auto Chess',
+            icon_url: 'https://raw.githubusercontent.com/arnaudgregoire/pokemonAutoChess/master/app/public/dist/assets/ui/logo-pac.png',
+          },
+        };
+        
+        message.reply({ embed: exampleEmbed });
+      });
+    });
   }
 
   else if (command === "whoisthebestdev") {
@@ -125,7 +212,7 @@ client.on("message", function(message) {
         let userNames = '';
         let levels = '';
         let wins = '';
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 20; i++) {
           const user = users[i];
 
           userNames += `\`${i + 1}\` ${user.email.split('@')[0]}\n`;
@@ -135,7 +222,7 @@ client.on("message", function(message) {
 
         const embed = new Discord.MessageEmbed()
           .setAuthor('Pokemon Auto Chess Leaderboard', 'https://raw.githubusercontent.com/arnaudgregoire/pokemonAutoChess/master/app/public/dist/assets/ui/logo-pac.png','https://pokemon-auto-chess.herokuapp.com/')
-          .addFields({ name: 'Top 10', value: userNames, inline: true },
+          .addFields({ name: 'Top 20', value: userNames, inline: true },
             { name: 'Level', value: levels, inline: true },
             { name: 'Wins', value: wins, inline: true })
           .setTimestamp()
