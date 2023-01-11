8069070f0b5b6e9475e2243c3d7601be11e1b9f2
keldaan <arnaudgregoire92@gmail.com>
2023-01-11 21:52:42 +0100

    limit to 180

--- commands/activity.js
+++ commands/activity.js
@@ -11,14 +11,14 @@ module.exports = {
 		.addStringOption(option =>
 			option
 				.setName('days')
-				.setDescription('The data range in days [0-365]')
+				.setDescription('The data range in days [0-180]')
 				.setRequired(true)),
 	async execute(interaction) {
         await interaction.deferReply();
         const input = parseInt(interaction.options.getString('days'))
         let numberOfDays = input ? input : 10
         numberOfDays = Math.max(0, input)
-        numberOfDays = Math.min(365, input)
+        numberOfDays = Math.min(180, input)
 
         let days = []
         let daysString = []
