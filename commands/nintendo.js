72292312b5d44e9e3caefcc24a460d98b4f1a626
keldaan <arnaudgregoire92@gmail.com>
2023-02-03 12:18:22 +0100

    fix phrasing

--- commands/nintendo.js
+++ commands/nintendo.js
@@ -5,6 +5,6 @@ module.exports = {
     .setName("nintendo")
     .setDescription("About The Pokemon Company Copyrights"),
   async execute(interaction) {
-    await interaction.reply("All rights to the Pokemon Company. Pokemon Auto Chess can stop at any time, whenever The Pokemon Company wants. Play it while you can.There is no point in discussing this topic.")
+    await interaction.reply("All rights to the Pokemon Company. Pokemon Auto Chess can stop at any time, whenever The Pokemon Company wants. Play it while you can.")
   },
 }
