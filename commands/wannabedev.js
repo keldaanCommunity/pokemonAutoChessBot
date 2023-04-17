0e42bf6c730a73b8d0789bcb873623531b35ea23
keldaan <arnaudgregoire92@gmail.com>
2023-04-17 21:45:38 +0200

    add wannabedev command

---
+++ commands/wannabedev.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require("discord.js")
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("wannabedev")
+    .setDescription("About coding suggestions"),
+  async execute(interaction) {
+    await interaction.reply(`You are not authorized to write random code keyword telling me 'you should do something like that'. Don't tell me how to code my game. If you want to help, everything is open source and pull requests are welcome (https://github.com/keldaanInteractive/pokemonAutoChess).`)
+  },
+}
