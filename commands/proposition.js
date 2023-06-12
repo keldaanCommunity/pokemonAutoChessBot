45745d4f1075ec77a8b53f1153971b9f3e398e2d
keldaan <arnaudgregoire92@gmail.com>
2023-06-12 22:33:58 +0200

    update guidelines

--- commands/proposition.js
+++ commands/proposition.js
@@ -5,6 +5,6 @@ module.exports = {
     .setName("proposition")
     .setDescription("Proposition guidelines"),
   async execute(interaction) {
-    await interaction.reply("Your pokemon proposition MUST includes all statistics, rarity, synergies, ability with detailed effect. You must also check sprite availability at  https://sprites.pmdcollab.org/ .")
+    await interaction.reply("Your pokemon proposition MUST includes all statistics, rarity, synergies, ability with detailed effect, attack animations name. You must also check sprite availability at  https://sprites.pmdcollab.org/. If you already find attack sprites (for example, a fireball for a flamethrower ability), it is very much appreciated.")
   },
 }
