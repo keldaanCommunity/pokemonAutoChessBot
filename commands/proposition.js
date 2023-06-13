7ccc90d92ad18e1499304d05d45e515d76d875d7
keldaan <arnaudgregoire92@gmail.com>
2023-06-13 18:37:38 +0200

    attack sprites links

--- commands/proposition.js
+++ commands/proposition.js
@@ -5,6 +5,6 @@ module.exports = {
     .setName("proposition")
     .setDescription("Proposition guidelines"),
   async execute(interaction) {
-    await interaction.reply("Your pokemon proposition MUST includes all statistics, rarity, synergies, ability with detailed effect, attack animations name. You must also check sprite availability at  https://sprites.pmdcollab.org/. If you already find attack sprites (for example, a fireball for a flamethrower ability), it is very much appreciated.")
+    await interaction.reply("Your pokemon proposition MUST includes all statistics, rarity, synergies, ability with detailed effect, attack animations name. You must also check sprite availability at https://sprites.pmdcollab.org/. If you find attack sprites (for example, a fireball for a flamethrower ability), it is very much appreciated. You can find attack sprites here: https://www.spriters-resource.com/ds_dsi/pokemonranger2shadowsofalmia/ https://www.spriters-resource.com/ds_dsi/pokemonmysterydungeonexplorersofsky/sheet/85692/ https://www.spriters-resource.com/ds_dsi/pokemonranger3guardiansigns/sheet/41335/")
   },
 }
