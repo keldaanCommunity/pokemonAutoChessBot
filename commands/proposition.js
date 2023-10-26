0178c1d8a83c43a9a0ec90bc4112dc971ce05dfd
keldaan <arnaudgregoire92@gmail.com>
2023-10-26 11:09:18 +0200

    new guidelines

--- commands/proposition.js
+++ commands/proposition.js
@@ -5,6 +5,15 @@ module.exports = {
     .setName("proposition")
     .setDescription("Proposition guidelines"),
   async execute(interaction) {
-    await interaction.reply({files: ["https://cdn.discordapp.com/attachments/1146829002493923338/1162004328547102811/keldaan.png?ex=653a5bdc&is=6527e6dc&hm=1b7445f89dc506430ffdfabca38ec55b688b72777ea8d9e1980a1a0eee2991eb&"]})
+    await interaction.reply(`To get your favorite pokemon in the game, your proposal must contain all of the following elements (will be deleted otherwise):
+    - Sprite links for every form/evolution (you can check availability at https://sprites.pmdcollab.org/). If sprites are not available, proposition will be deleted.
+    - For every form/evolution, write the name of the animation you want for Attack/Ability/Emote. Missing animations proposition will be deleted.
+    - Every pokemon statistics for every form/evolution: Additional pool ?, Rarity, Heath, Attack, Defense Special, Defense, Attack range, PP. Missing statistic proposition will be deleted.
+    - Take a look at what are the statistic of similar pokemons. Non sense statistic will be deleted.
+    - Precise description of the ability, including every evolution damage, evolution scaling, and every other possible effects. Incomplete ability will be delted.
+    - Be creative with your ability idea. Low effort idea will be deleted.
+    - If your pokemon has a passive, write the most precise description of it.
+    - Bonus if you can find attack particles sprites. (https://github.com/PMDCollab/RawAsset/tree/master/Particle, https://www.spriters-resource.com/ds_dsi/pokemonmysterydungeonexplorersofsky/sheet/85692/)
+    `)
   },
 }
