f11be3ba3bb4909168b5f71e7a2eed42bd89e4bc
keldaan <arnaudgregoire92@gmail.com>
2023-10-26 11:10:15 +0200

    fix types

--- commands/proposition.js
+++ commands/proposition.js
@@ -8,7 +8,7 @@ module.exports = {
     await interaction.reply(`To get your favorite pokemon in the game, your proposal must contain all of the following elements (will be deleted otherwise):
     - Sprite links for every form/evolution (you can check availability at https://sprites.pmdcollab.org/). If sprites are not available, proposition will be deleted.
     - For every form/evolution, write the name of the animation you want for Attack/Ability/Emote. Missing animations proposition will be deleted.
-    - Every pokemon statistics for every form/evolution: Additional pool ?, Rarity, Heath, Attack, Defense Special, Defense, Attack range, PP. Missing statistic proposition will be deleted.
+    - Every pokemon statistics for every form/evolution: Additional pool ?, Types, Rarity, Heath, Attack, Defense Special, Defense, Attack range, PP. Missing statistic proposition will be deleted.
     - Take a look at what are the statistic of similar pokemons. Non sense statistic will be deleted.
     - Precise description of the ability, including every evolution damage, evolution scaling, and every other possible effects. Incomplete ability will be delted.
     - Be creative with your ability idea. Low effort idea will be deleted.
