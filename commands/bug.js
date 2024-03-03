7d09bd9694cf399568777bf394e00f2e573a572a
Arnaud GREGOIRE <arnaud.gregoire@ptvgroup.com>
2024-03-03 18:25:32 +0100

    imgur host

--- commands/bug.js
+++ commands/bug.js
@@ -4,9 +4,7 @@ module.exports = {
   data: new SlashCommandBuilder().setName("bug").setDescription("Report a bug"),
   async execute(interaction) {
     await interaction.reply({
-      files: [
-        "https://cdn.discordapp.com/attachments/1063164968721190974/1213895276784259092/bug-report-guide.png?ex=65f72313&is=65e4ae13&hm=c212dfa293cd5d5ef9c891649f7fbd3c1a20dab2b70eab443abf6d54828edc94&",
-      ],
+      files: ["https://i.imgur.com/b1R7cAr.png"],
     });
   },
 };
