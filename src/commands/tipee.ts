4328b0debb9be653147940bdccb892e459886ea2
Keldaan <keldaan.ag@gmail.com>
2025-03-03 10:31:47 +0100

    new tipee command

--- src/commands/tipee.ts
+++ src/commands/tipee.ts
@@ -6,7 +6,7 @@ module.exports = {
 		.setDescription("About donations and tipee"),
 	async execute(interaction) {
 		await interaction.reply(
-			"You can support the project on https://en.tipeee.com/keldaan/. Pokemon Auto Chess is and will remain a non profit game. Every dollar of this tipee is used for server/database hosting together with commissions for spriters.",
+			"Are you experiencing technical problems playing pokemon auto chess? Before you get too excited, take a few seconds to ask yourself how such a game works. Pokemon Auto Chess servers may not be the most stable or the most efficient. Why can't they be? Because hosting a game with over 10,000 daily users costs money (all server costs are public https://discord.com/channels/737230355039387749/1268146688313266288). How is this financed ? With donations. You can support the project on https://en.tipeee.com/keldaan/. Pokemon Auto Chess is and will remain a non profit game. Every dollar of this tipee is used for server/database hosting.",
 		);
 	},
 };
